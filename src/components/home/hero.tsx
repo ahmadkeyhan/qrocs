2fc41207ee39b36226e6b045173805b525f784c2
keyhansa <keyhan.ahmad@gmail.com>
2025-04-19 11:18:47 +0330

    added banner to hero and changed tablet h1 size

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,12 +1,17 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import {QrCode} from "lucide-react"
+import { useTheme } from "next-themes";
 
 export default function Hero() {
+    const {theme} = useTheme()
+    console.log(theme)
   return (
-    <div className="grid gap-8 sm:grid-cols-2 w-full p-4 max-w-6xl mb-6">
+    <div className="grid sm:grid-cols-2 w-full p-4 max-w-5xl">
         <div className="flex flex-col text-center px-8 sm:px-2 items-center justify-center w-full">
-            <h1 className="text-3xl lg:text-5xl text-primary mb-1">منوی دیجیتالتو بساز</h1>
+            <h1 className="text-3xl md:text-4xl lg:text-5xl text-primary mb-1">منوی دیجیتالتو بساز</h1>
             <p className="text-lg mb-4 text-foreground w-[32ch]">جای اینکه هر ماه منوتون رو دوباره چاپ کنید، با کراکس منوی کافه یا رستوران رو دیجیتال کنید تا مشتریانتون با اسکن کیوآرکد به راحتی ببیننش!</p>
             <div className="flex justify-center gap-4">
                 <Button variant="default" size="lg">
@@ -18,9 +23,9 @@ export default function Hero() {
                 </Button>
             </div>
         </div>
-        <div className="relative aspect-[5/4] w-full rounded-3xl overflow-hidden">
+        <div className="relative aspect-square w-full -my-4 lg:-my-10">
             <Image
-                src="/placeholder.svg"
+                src={theme === "light" ? `/heroBanner.png` : `/heroBannerDark.png`}
                 alt="sdf"
                 fill
                 className="object-cover"
